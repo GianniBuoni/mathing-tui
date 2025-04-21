d0016590efb32266481007ad26785ebe14ec398a
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-04-21 15:59:51 -0400

    🧹 rename variables
    - queries that affect one row should be singular

--- src/db/store_items/queries.rs
+++ src/db/store_items/queries.rs
@@ -28,7 +28,7 @@ pub async fn get_item_single(
     Ok(item)
 }
 
-pub async fn add_items(
+pub async fn add_item(
     conn: &SqlitePool,
     name: &str,
     price: f64,
@@ -57,7 +57,7 @@ pub async fn add_items(
     Ok(new_item)
 }
 
-pub async fn delete_items(
+pub async fn delete_item(
     conn: &SqlitePool,
     id: i64,
 ) -> Result<(), Box<dyn Error>> {
@@ -67,7 +67,7 @@ pub async fn delete_items(
     Ok(())
 }
 
-pub async fn update_items(
+pub async fn update_item(
     conn: &SqlitePool,
     id: i64,
     name: Option<&str>,
--- src/db/store_items/tests.rs
+++ src/db/store_items/tests.rs
@@ -14,7 +14,7 @@ const TEST_ITEMS: [(&str, f64); 3] = [
 #[sqlx::test]
 async fn test_add_items(conn: SqlitePool) -> Result<(), Box<dyn Error>> {
     for item in TEST_ITEMS {
-        let test_item = add_items(&conn, item.0, item.1).await?;
+        let test_item = add_item(&conn, item.0, item.1).await?;
         assert_eq!(item.0, test_item.name(), "Test new item's name match.");
         assert_eq!(item.1, test_item.price(), "Test new item's price match.");
     }
@@ -39,7 +39,7 @@ async fn test_get_item_single(conn: SqlitePool) -> Result<(), Box<dyn Error>> {
     let mut ids = vec![];
 
     for (name, price) in TEST_ITEMS {
-        let new_items = add_items(&conn, name, price).await?;
+        let new_items = add_item(&conn, name, price).await?;
         ids.push(new_items.id());
     }
 
@@ -58,8 +58,8 @@ async fn test_delete_items(conn: SqlitePool) -> Result<(), Box<dyn Error>> {
     let original_len = get_items(&conn).await?.len();
 
     for item in TEST_ITEMS {
-        let new_item = add_items(&conn, item.0, item.1).await?;
-        delete_items(&conn, new_item.id()).await?;
+        let new_item = add_item(&conn, item.0, item.1).await?;
+        delete_item(&conn, new_item.id()).await?;
     }
 
     let final_len = get_items(&conn).await?.len();
@@ -73,7 +73,7 @@ async fn test_update_items(conn: SqlitePool) -> Result<(), Box<dyn Error>> {
     let mut ids = vec![];
 
     for (name, price) in TEST_ITEMS {
-        let new_item = add_items(&conn, name, price).await?;
+        let new_item = add_item(&conn, name, price).await?;
         ids.push(new_item.id());
     }
 
@@ -85,7 +85,7 @@ async fn test_update_items(conn: SqlitePool) -> Result<(), Box<dyn Error>> {
 
     for (id, (name, price)) in ids.iter().zip(update_params) {
         sleep_until(Instant::now() + Duration::from_secs(1)).await;
-        update_items(&conn, *id, name, price).await?;
+        update_item(&conn, *id, name, price).await?;
     }
 
     let want = [
