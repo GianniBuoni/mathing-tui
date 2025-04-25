e52cd7f708a65cc4aa37d114e9117bf8ebfa0ac5
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-04-25 15:05:09 -0400

    🧪 another test cleanup
    - removed need to make temporary variables by iterating in place

--- src/db/tests/store_receits_users_init.rs
+++ src/db/tests/store_receits_users_init.rs
@@ -1,58 +1,42 @@
-use std::{collections::HashMap, default};
-
 use super::*;
 
 pub async fn init_test(conn: &SqlitePool) -> Result<(), Box<dyn Error>> {
     let mut r_ids = vec![];
 
-    for (name, price, qty) in TEST_ITEMS {
+    for ((name, price, qty), user) in TEST_ITEMS.into_iter().zip(TEST_USERS) {
+        add_store_user(conn, user).await?;
         let item_row = add_store_item(conn, name, price).await?;
         let receipt_row = add_store_receipt(conn, item_row.id, qty).await?;
         r_ids.push(receipt_row.id);
     }
 
-    let mut u_ids = vec![];
-
-    for name in TEST_USERS {
-        let user_row = add_store_user(conn, name).await?;
-        u_ids.push(user_row.id);
-    }
-
-    let err = "Wrong id/user match check how you add users into db.";
-    let jon = get_store_user_single(conn, 3).await?;
-    if jon.name != TEST_USERS[2] {
-        return Err(err.into());
-    }
+    let err = "Wrong id/user match; check how you add users into db.";
     let noodle = get_store_user_single(conn, 2).await?;
     if noodle.name != TEST_USERS[1] {
         return Err(err.into());
     }
-
-    let mut pairs = HashMap::new();
-    let mut r_ids = r_ids.iter();
-    // Add "Jon" to PB Pretzel, 2
-    if let Some(id) = r_ids.next() {
-        let v = vec![jon.id];
-        pairs.insert(*id, v);
+    let jon = get_store_user_single(conn, 3).await?;
+    if jon.name != TEST_USERS[2] {
+        return Err(err.into());
     }
 
-    // Add "Noodle" to Slamin' Salmon, 1
-    if let Some(id) = r_ids.next() {
-        let v = vec![noodle.id];
-        pairs.insert(*id, v);
-    }
-
-    // Add "Noodle" and "Jon"  to Chips and Dip, 3
-    if let Some(id) = r_ids.next() {
-        let v = vec![jon.id, noodle.id];
-        pairs.insert(*id, v);
-    }
-
-    for (r_id, u_ids) in pairs.iter() {
-        for u_id in u_ids {
-            add_store_receipts_users(&conn, *r_id, *u_id).await?;
-        }
-    }
+    try_join_all(r_ids.into_iter().map(async |r_id| {
+        Ok::<(), Box<dyn Error>>({
+            let uids = match r_id {
+                1 => vec![jon.id],
+                2 => vec![noodle.id],
+                3 => vec![noodle.id, jon.id],
+                _ => vec![],
+            };
+            try_join_all(uids.into_iter().map(async |u_id| {
+                Ok::<(), Box<dyn Error>>({
+                    add_store_receipts_users(conn, r_id, u_id).await?
+                })
+            }))
+            .await?;
+        })
+    }))
+    .await?;
 
     Ok(())
 }
