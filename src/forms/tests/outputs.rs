e1fcef011d73469695b2a1cc3fa0fdf4da4b87bb
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-05-26 16:14:23 -0400

    🧪 test: test validation errors

--- src/forms/tests/outputs.rs
+++ src/forms/tests/outputs.rs
@@ -12,13 +12,51 @@ fn test_form_validation() -> Result<()> {
     ];
 
     let mut form = test_full_form();
-
-    key_events.into_iter().for_each(|key| {
-        let action = Some(key);
-        form.update(action);
-    });
+    key_events.iter().for_each(|key| form.update(Some(*key)));
 
     let form = FormTui::ItemForm(form);
     form.validate()?;
     Ok(())
+}
+
+#[test]
+fn test_invalid_data() {
+    let key_events = [
+        Action::HandleInput(KeyEvent::from(KeyCode::Char('a'))),
+        Action::SelectForward,
+        Action::HandleInput(KeyEvent::from(KeyCode::Char('a'))),
+    ];
+
+    let mut form = test_full_form();
+    key_events.iter().for_each(|key| form.update(Some(*key)));
+
+    let form = FormTui::ItemForm(form);
+    match form.validate() {
+        Ok(_) => panic!("expected an Error"),
+        Err(got) => {
+            let want = "Item price is not a float.";
+            assert_eq!(want, AsRef::<str>::as_ref(&got.to_string()))
+        }
+    }
+}
+
+#[test]
+fn test_unset_data() {
+    let key_events = [
+        Action::SelectForward,
+        Action::HandleInput(KeyEvent::from(KeyCode::Char('a'))),
+        Action::HandleInput(KeyEvent::from(KeyCode::Char('a'))),
+    ];
+
+    let mut form = test_full_form();
+    key_events.iter().for_each(|key| form.update(Some(*key)));
+    let form = FormTui::ItemForm(form);
+
+    match form.validate() {
+        Ok(_) => panic!("expected an Error"),
+        Err(got) => {
+            let want = "Item Name is unset.";
+            assert_eq!(want, AsRef::<str>::as_ref(&got.to_string()))
+        }
+    }
 }
