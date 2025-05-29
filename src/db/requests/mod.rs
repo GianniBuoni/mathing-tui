64b680a64b31563d34a953df2e03f03f32cf33d6
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-05-29 14:42:57 -0400

    feat: add more param stucts for db types

--- src/db/requests/mod.rs
+++ src/db/requests/mod.rs
@@ -1,8 +1,10 @@
+use std::borrow::Cow;
+
 use super::*;
 
 pub mod prelude {
     pub use super::{
-        DbPayload, DbRequest, DbResponse, ItemParams, ReceiptParams,
+        DbPayload, DbRequest, DbResponse, ItemParams, JoinedReceiptParams,
         RequestType,
     };
 }
@@ -12,39 +14,58 @@ mod item_params;
 pub trait Request<T> {
     async fn get(&self) -> Result<T>;
     async fn post(&self) -> Result<T>;
-    async fn update(&self) -> Result<()>;
-    async fn delete(&self) -> Result<()>;
+    async fn update(&self) -> Result<T>;
+    async fn delete(&self) -> Result<T>;
 }
 
 #[derive(Debug, Default)]
-pub struct ItemParams {
+pub struct UserParams<'db> {
+    u_id: Option<i64>,
+    name: Option<Cow<'db, str>>,
+}
+
+#[derive(Debug, Default)]
+pub struct ItemParams<'db> {
     item_id: Option<i64>,
-    item_name: Option<String>,
+    item_name: Option<Cow<'db, str>>,
     item_price: Option<f64>,
 }
 
 #[derive(Debug, Default)]
-pub struct ReceiptParams {
-    pub id: Option<i64>,
-    pub item_id: Option<i64>,
-    pub item_qty: Option<i64>,
-    pub users: Vec<i64>,
+pub struct JoinedReceiptParams {
+    r_id: Option<i64>,
+    item_id: Option<i64>,
+    item_qty: Option<i64>,
+    users: Vec<i64>,
 }
 
-pub struct DbResponse {
+#[derive(Debug, Default)]
+struct ReceiptParams {
+    r_id: Option<i64>,
+    item_id: Option<i64>,
+    item_qty: Option<i64>,
+}
+
+#[derive(Debug, Default)]
+pub struct ReceiptsUsersParams {
+    r_id: Option<i64>,
+    u_id: Option<i64>,
+}
+
+pub struct DbResponse<'db> {
     pub req_type: RequestType,
-    pub payload: DbPayload,
+    pub payload: DbPayload<'db>,
     pub error: Option<String>,
 }
 
-pub struct DbRequest {
+pub struct DbRequest<'db> {
     pub req_type: RequestType,
-    pub payload: DbPayload,
+    pub payload: DbPayload<'db>,
 }
 
-pub enum DbPayload {
-    ItemParams(ItemParams),
-    ReceiptParamss(ReceiptParams),
+pub enum DbPayload<'db> {
+    ItemParams(ItemParams<'db>),
+    ReceiptParams(JoinedReceiptParams),
     Item(StoreItem),
     Receipt(StoreJoinRow),
 }
