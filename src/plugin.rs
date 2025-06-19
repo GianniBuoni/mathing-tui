8cc8072fc26b6443e322dc91333dfd36c861fc22
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-06-19 10:40:56 -0400

    feat: more constraints for the Plugin traits

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,31 +1,28 @@
-use anyhow::Result;
-
-use crate::prelude::{Component, ComponentTracker};
+use crate::prelude::{Component, ComponentBuilder, ComponentTracker};
 
 pub mod prelude {
-    pub use super::{Plugin, PluginParent};
+    pub use super::{Plugin, PluginInit, PluginParent};
 }
 
 /// Descripes a parent child relationship between Components
-pub trait Plugin: Component + Sized {
-    type Parent: PluginParent;
+pub trait Plugin: Component + PluginInit + Sized {
+    type Parent: PluginParent + ComponentBuilder;
 
     // required methods
-    /// plugin() is called and is primarily responsible for
-    /// adding itself to the parent component.
-    /// Any other pieces that the child has that the parent needs are
-    /// called handled here as well.
+    /// plugin() is primarily responsible for adding itself to the
+    /// parent component. Any other pieces that the child has that
+    /// the parent needs are called and handled here as well.
     fn plugin(self, parent: &mut Self::Parent);
+}
+
+pub trait PluginInit {
     /// init is called when the parent is fully ready to be built:
-    /// all child -> parent relationships should be defined.
+    /// all child -> parent relationships should be defined in plugin().
     /// the init resovles any parent -> child relationships.
     fn init(&mut self, index: usize, tracker: ComponentTracker);
 }
 
 pub trait PluginParent {
-    // required method
-    fn init(&mut self) -> Result<()>;
-
     // provided method
     fn add_plugins(&mut self, plugin: fn(&mut Self)) -> &mut Self {
         plugin(self);
