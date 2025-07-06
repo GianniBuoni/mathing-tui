70681dbbfeb77d3d3308b85b4b8bc6441ab62bce
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-07-06 12:36:37 -0400

    refactor: move big sql statement to sql/folder

--- src/config/store.rs
+++ src/config/store.rs
@@ -7,46 +7,8 @@ impl DbConn {
         ))?;
         let pool = SqlitePool::connect(db_str).await?;
 
-        sqlx::query!(
-            "
-CREATE TABLE IF NOT EXISTS items (
-  id INTEGER PRIMARY KEY NOT NULL,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL,
-  name TEXT NOT NULL,
-  price FLOAT NOT NULL
-);
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY NOT NULL,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL,
-  name TEXT NOT NULL
-);
-CREATE TABLE IF NOT EXISTS receipts(
-  id INTEGER PRIMARY KEY NOT NULL,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL,
-  item_id INTEGER NOT NULL,
-  item_qty INTEGER NOT NULL,
-  FOREIGN KEY(item_id) REFERENCES items(id)
-    ON DELETE CASCADE
-);
-CREATE TABLE IF NOT EXISTS receipts_users (
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL,
-  receipt_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  FOREIGN KEY(receipt_id) REFERENCES receipts(id)
-    ON DELETE CASCADE,
-  FOREIGN KEY(user_id) REFERENCES users(id)
-    ON DELETE CASCADE,
-  UNIQUE(receipt_id, user_id)
-);
-            "
-        )
-        .execute(&pool)
-        .await?;
-
+        // run migrations if tabless don't exist
+        sqlx::query_file!("sql/init.sql").execute(&pool).await?;
         Ok(Self(pool))
     }
 
