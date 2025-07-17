59dcfd31ba2472338d3c12e42c447dfdefca8fe8
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-07-17 15:38:13 -0400

    refactor: restructure form submission
    - create try_add_conditional_req method to encapsulate logic for
      deciidng what payloads trigger addiitonal requeests
    - remove redundant logic from the try_submit callbacks
    - add call to get conditional reqs to the main submission method

--- src/home/form_submission.rs
+++ src/home/form_submission.rs
@@ -12,8 +12,14 @@ impl Home {
                     return Aok(());
                 }
             };
-            let requests = submission_callback(self)?;
-            requests.into_iter().try_for_each(|f| self.try_send(f))?;
+            let mut reqs = submission_callback(self)?;
+            let first_req = reqs.first().unwrap();
+            let (app_arm, req_type) = (
+                TryInto::<AppArm>::try_into(&first_req.payload)?,
+                first_req.req_type,
+            );
+            self.try_add_conditional_reqs(&mut reqs, app_arm, req_type)?;
+            reqs.into_iter().try_for_each(|f| self.try_send(f))?;
 
             Aok(())
         })() {
@@ -32,84 +38,30 @@ impl Home {
     }
     // helper methods
     fn try_form_submit(&mut self) -> Result<Vec<DbRequest>> {
-        // unwrap the form. Should only be called if there is a form
-        // to opperate on.
         let form = &mut self
             .form
             .as_mut()
             .ok_or(ComponentError::not_found("form"))?;
         form.submit()?;
 
-        // initialize all the return data
-        let mut requests = vec![DbRequest::new()];
-        let req_type = form.get_req_type();
-        let payload = form.try_get_payload()?;
-
-        // if there is un update, check if we need to do
-        // some calculations or push a refresh request.
-        match (&payload, req_type) {
-            (_, RequestType::Post) => {
-                requests.append(&mut DbRequest::counts());
-            }
-            (DbPayload::ReceiptParams(_), RequestType::Update) => {
-                self.try_subtract_store_total()?
-            }
-            (
-                DbPayload::ItemParams(_) | DbPayload::UserParams(_),
-                RequestType::Update,
-            ) => {
-                requests.append(&mut self.try_collect_refresh_reqs()?);
-            }
-            _ => {}
-        }
-        // update original request to consume the payload and
-        // request type
-        requests
-            .first_mut()
-            .unwrap()
-            .with_req_type(req_type)
-            .with_payload(payload);
-
-        Ok(requests)
+        Ok(vec![
+            DbRequest::new()
+                .with_req_type(form.get_req_type())
+                .with_payload(form.try_get_payload()?),
+        ])
     }
-
     fn try_dialogue_submit(&mut self) -> Result<Vec<DbRequest>> {
         let dialogue = self
             .message
             .as_mut()
-            .ok_or(ComponentError::not_found("message"))?;
+            .ok_or(ComponentError::not_found("Message"))?;
 
-        let mut requests = vec![DbRequest::new()];
-        let req_type = dialogue.get_req_type();
-        let payload = dialogue.try_get_payload()?;
-
-        match (&payload, req_type) {
-            (DbPayload::ReceiptParams(_), RequestType::Delete) => {
-                self.try_subtract_store_total()?;
-                requests.append(&mut DbRequest::counts())
-            }
-            (DbPayload::ItemParams(_), RequestType::Delete) => {
-                requests.append(&mut self.try_collect_refresh_reqs()?);
-                requests.append(&mut DbRequest::counts())
-            }
-            (_, RequestType::Delete) => {
-                requests.append(&mut DbRequest::counts())
-            }
-            (DbPayload::StoreTotal, _ /*Refresh to app init*/) => {
-                requests.append(&mut DbRequest::init())
-            }
-            _ => {}
-        }
-
-        requests
-            .first_mut()
-            .unwrap()
-            .with_req_type(req_type)
-            .with_payload(payload);
-
-        Ok(requests)
+        Ok(vec![
+            DbRequest::new()
+                .with_req_type(dialogue.get_req_type())
+                .with_payload(dialogue.try_get_payload()?),
+        ])
     }
-
     fn try_collect_refresh_reqs(&self) -> Result<Vec<DbRequest>> {
         let mut refectch_reqs = Vec::with_capacity(4);
         refectch_reqs.push(DbRequest::refresh());
@@ -117,9 +69,54 @@ impl Home {
         self.components
             .iter()
             .try_fold(refectch_reqs, |mut acc, f| {
-                let req = f.try_get_refresh()?;
-                acc.push(req);
+                if let Some(req) = f.get_refresh_reqs() {
+                    acc.push(req);
+                };
                 Aok(acc)
             })
+    }
+    fn try_add_conditional_reqs(
+        &mut self,
+        reqs: &mut Vec<DbRequest>,
+        app_arm: AppArm,
+        req_type: RequestType,
+    ) -> Result<()> {
+        match (app_arm, req_type) {
+            // Post conditions
+            (_, RequestType::Post) => {
+                (|| {
+                    let Some(table) = self.get_mut_table_from_type(app_arm)
+                    else {
+                        return;
+                    };
+                    if let Some(req) = table.goto_last_page() {
+                        reqs.push(req);
+                        reqs.reverse();
+                    }
+                })();
+                reqs.append(&mut DbRequest::counts())
+            }
+            // Update conditions
+            (AppArm::Receipts, RequestType::Update) => {
+                self.try_subtract_store_total()?
+            }
+            (AppArm::Items | AppArm::Users, RequestType::Update) => {
+                reqs.append(&mut self.try_collect_refresh_reqs()?)
+            }
+            // Deletion Conditions
+            (_, RequestType::Delete) => {
+                match app_arm {
+                    AppArm::Items | AppArm::Users => {
+                        reqs.append(&mut self.try_collect_refresh_reqs()?)
+                    }
+                    AppArm::Receipts => self.try_subtract_store_total()?,
+                    _ => {}
+                }
+                reqs.append(&mut DbRequest::counts())
+            }
+            (AppArm::Totals, _) => reqs.append(&mut DbRequest::init()),
+            _ => return Ok(()),
+        }
+        Ok(())
     }
 }
