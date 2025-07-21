13d9cf6282ac84718da7af9438c83aed8426a9ed
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-07-21 18:52:36 -0400

    refactor: change form building closures
    - closures simply return Aok's w/o the need for turbo fish
    - part of an ongoing effort to use simpler(?) syntax t/o

--- src/home/form_building.rs
+++ src/home/form_building.rs
@@ -3,9 +3,8 @@ use super::*;
 impl Home {
     pub(super) fn enter_insert(&mut self) {
         match (|| {
-            Aok::<Option<Form>>({
-                self.try_get_current_table()?.try_new_form()?
-            })
+            let form = self.try_get_current_table()?.try_new_form()?;
+            Aok(form)
         })() {
             Ok(form) => {
                 if let Some(form) = form {
@@ -17,11 +16,7 @@ impl Home {
         }
     }
     pub(super) fn delete_selected(&mut self) {
-        match (|| {
-            Aok::<Option<Dialogue>>({
-                self.try_get_current_table()?.try_delete_form()?
-            })
-        })() {
+        match (|| self.try_get_current_table()?.try_delete_form())() {
             Ok(log) => {
                 if let Some(log) = log {
                     self.message = Some(log);
@@ -33,15 +28,14 @@ impl Home {
     }
     pub(super) fn edit_selected(&mut self) {
         match (|| {
-            Aok::<Option<Form>>({
-                let table = self.try_get_current_table()?;
-                if let Some(AppArm::Receipts) = table.table_type {
-                    let (r, users) = self.edit_r_params()?;
-                    Form::edit_receipt(r, users).map(Some)?
-                } else {
-                    table.edit_form()?
-                }
-            })
+            let table = self.try_get_current_table()?;
+            let form = if let Some(AppArm::Receipts) = table.table_type {
+                let (r, users) = self.edit_r_params()?;
+                Form::edit_receipt(r, users).map(Some)?
+            } else {
+                table.edit_form()?
+            };
+            Aok(form)
         })() {
             Ok(form) => {
                 if let Some(form) = form {
