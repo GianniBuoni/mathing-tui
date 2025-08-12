0b8bad8a8c5c08cb3fe95fc7ca51a39b8fab23ef
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-08-12 12:56:26 -0400

    chore: add build flags to package
    - add result folder to gitignore

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
       buildInputs = [pkgs.openssl];
       nativeBuildInputs = [pkgs.pkg-config];
       cargoLock.lockFile = ./Cargo.lock;
+      cargoBuildFlags = "--bin mathing";
     };
   };
 }
