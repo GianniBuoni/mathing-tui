de5244bf00851b61d30fe834cc92906340463317
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-08-12 11:42:07 -0400

    chore: package for NixOS

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Mathing: Expense splitting in the terminal!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    packages."x86_64-linux".default = pkgs.rustPlatform.buildRustPackage {
+      name = "mathing";
+      src = ./.;
+      buildInputs = [pkgs.openssl];
+      nativeBuildInputs = [pkgs.pkg-config];
+      cargoLock.lockFile = ./Cargo.lock;
+    };
+  };
+}
