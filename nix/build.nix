eac8f335c18b0b64fc16a35023c14ada9910a179
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-10-28 20:34:03 -0400

    🚧 refactor: remove comments froms build definition

--- nix/build.nix
+++ nix/build.nix
@@ -15,11 +15,8 @@
       src = lib.cleanSourceWith {
         src = inputs.self;
         filter = path: type:
-        # add `.sqlx/*` to project source files
           (lib.hasInfix ".sqlx/" path)
-          # add `sql` files to project src
           || (lib.hasSuffix ".sql" path)
-          # default crane project src filter
           || (config.rust-project.crane-lib.filterCargoSources path type);
       };
       crates.mathing.crane = {
